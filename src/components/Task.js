bb032ede140f3bcbabde484efbe7f300d19d8024
Vlad Mitroshin <vlad.mitroshin.99@gmail.com>
2021-12-20 15:50:41 +0200

    Create delete and toggle reminder functions for tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,17 @@
 import { FaTimes } from "react-icons/fa";
 
-const Task = ({ task }) => {
+const Task = ({ task, onDelete, onToggle }) => {
   return (
-    <div className="task">
+    <div
+      className={`task ${task.reminder ? "reminder" : ""}`}
+      onDoubleClick={() => onToggle(task.id)}
+    >
       <h3>
-        {task.text} <FaTimes className="task__remove-task" />
+        {task.text}
+        <FaTimes
+          className="task__remove-task"
+          onClick={() => onDelete(task.id)}
+        />
       </h3>
       <p>
         {task.day}, {task.time}
